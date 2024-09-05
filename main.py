32f967a2c1a9963f3a3e19c3d6b03da57a0455f7
Edgily <endelean0a@outlook.com>
2024-09-05 18:46:36 +0100

    made pygame render a black screen

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import pygame
 from constants import *
 
 def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+	while True:
+		pygame.Surface.fill(screen, (0, 0, 0, 1))
+		pygame.display.flip()
+		
+		# check for window close, end process if closed
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+					return
 
 if __name__ == "__main__":
 	main()
